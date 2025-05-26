4dedf1c62ee2dd8b47ca99af9b4157b319f843c4
Leonardo <ljguzmand@gmail.com>
2025-05-26 01:46:19 -0300

    1er argumento GET

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import { argv } from 'node:process';
+
 console.log(`Hola Mundo`);
+
+console.log(argv);
+
+
+// FETCH
+fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+// Función GET
+if (argv[2].toLowerCase() == 'GET'.toLowerCase()) {
+    console.log('El argumento es GET');
+}
+
+// Función POST
+if (argv[2].toLowerCase() == 'POST'.toLowerCase()) {
+    console.log('El argumento es POST ' + argv[3]);
+}
+
+
